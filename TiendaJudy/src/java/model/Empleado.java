2b0399985df0cd3712994962b241849592e56f7f
Jonathan Ramirez <jonathan.ramirezz15@gmail.com>
2019-08-26 23:26:29 -0600

    .

---
+++ TiendaJudy/src/java/model/Empleado.java
@@ -0,0 +1,142 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+import java.io.Serializable;
+import java.util.List;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlTransient;
+
+/**
+ *
+ * @author toursys
+ */
+@Entity
+@Table(name = "EMPLEADO")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Empleado.findAll", query = "SELECT e FROM Empleado e")
+    , @NamedQuery(name = "Empleado.findByIdEmpleado", query = "SELECT e FROM Empleado e WHERE e.idEmpleado = :idEmpleado")
+    , @NamedQuery(name = "Empleado.findByNombre", query = "SELECT e FROM Empleado e WHERE e.nombre = :nombre")
+    , @NamedQuery(name = "Empleado.findByTelefono", query = "SELECT e FROM Empleado e WHERE e.telefono = :telefono")
+    , @NamedQuery(name = "Empleado.findByCorreo", query = "SELECT e FROM Empleado e WHERE e.correo = :correo")
+    , @NamedQuery(name = "Empleado.findBySalario", query = "SELECT e FROM Empleado e WHERE e.salario = :salario")})
+public class Empleado implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "ID_EMPLEADO")
+    private Integer idEmpleado;
+    @Size(max = 50)
+    @Column(name = "NOMBRE")
+    private String nombre;
+    @Size(max = 30)
+    @Column(name = "TELEFONO")
+    private String telefono;
+    @Size(max = 100)
+    @Column(name = "CORREO")
+    private String correo;
+    // @Max(value=?)  @Min(value=?)//if you know range of your decimal fields consider using these annotations to enforce field validation
+    @Column(name = "SALARIO")
+    private Double salario;
+    @OneToMany(mappedBy = "idEmpleado", fetch = FetchType.LAZY)
+    private List<EncabezadoFactura> encabezadoFacturaList;
+
+    public Empleado() {
+    }
+
+    public Empleado(Integer idEmpleado) {
+        this.idEmpleado = idEmpleado;
+    }
+
+    public Integer getIdEmpleado() {
+        return idEmpleado;
+    }
+
+    public void setIdEmpleado(Integer idEmpleado) {
+        this.idEmpleado = idEmpleado;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public Double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(Double salario) {
+        this.salario = salario;
+    }
+
+    @XmlTransient
+    public List<EncabezadoFactura> getEncabezadoFacturaList() {
+        return encabezadoFacturaList;
+    }
+
+    public void setEncabezadoFacturaList(List<EncabezadoFactura> encabezadoFacturaList) {
+        this.encabezadoFacturaList = encabezadoFacturaList;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (idEmpleado != null ? idEmpleado.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Empleado)) {
+            return false;
+        }
+        Empleado other = (Empleado) object;
+        if ((this.idEmpleado == null && other.idEmpleado != null) || (this.idEmpleado != null && !this.idEmpleado.equals(other.idEmpleado))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "model.Empleado[ idEmpleado=" + idEmpleado + " ]";
+    }
+    
+}
---
+++ TiendaJudy/src/java/model/EncabezadoFactura.java
@@ -0,0 +1,173 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+import java.io.Serializable;
+import java.util.List;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlTransient;
+
+/**
+ *
+ * @author toursys
+ */
+@Entity
+@Table(name = "ENCABEZADO_FACTURA")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "EncabezadoFactura.findAll", query = "SELECT e FROM EncabezadoFactura e")
+    , @NamedQuery(name = "EncabezadoFactura.findByIdEncabezadoFactura", query = "SELECT e FROM EncabezadoFactura e WHERE e.idEncabezadoFactura = :idEncabezadoFactura")
+    , @NamedQuery(name = "EncabezadoFactura.findByValorVenta", query = "SELECT e FROM EncabezadoFactura e WHERE e.valorVenta = :valorVenta")
+    , @NamedQuery(name = "EncabezadoFactura.findByDescuento", query = "SELECT e FROM EncabezadoFactura e WHERE e.descuento = :descuento")
+    , @NamedQuery(name = "EncabezadoFactura.findBySubtotal", query = "SELECT e FROM EncabezadoFactura e WHERE e.subtotal = :subtotal")
+    , @NamedQuery(name = "EncabezadoFactura.findByIva", query = "SELECT e FROM EncabezadoFactura e WHERE e.iva = :iva")
+    , @NamedQuery(name = "EncabezadoFactura.findByTotalVenta", query = "SELECT e FROM EncabezadoFactura e WHERE e.totalVenta = :totalVenta")})
+public class EncabezadoFactura implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "ID_ENCABEZADO_FACTURA")
+    private Integer idEncabezadoFactura;
+    // @Max(value=?)  @Min(value=?)//if you know range of your decimal fields consider using these annotations to enforce field validation
+    @Column(name = "VALOR_VENTA")
+    private Double valorVenta;
+    @Column(name = "DESCUENTO")
+    private Double descuento;
+    @Column(name = "SUBTOTAL")
+    private Double subtotal;
+    @Column(name = "IVA")
+    private Double iva;
+    @Column(name = "TOTAL_VENTA")
+    private Double totalVenta;
+    @OneToMany(mappedBy = "idEncabezadoFactura", fetch = FetchType.LAZY)
+    private List<DetalleFactura> detalleFacturaList;
+    @JoinColumn(name = "ID_CLIENTE", referencedColumnName = "ID_CLIENTE")
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Cliente idCliente;
+    @JoinColumn(name = "ID_EMPLEADO", referencedColumnName = "ID_EMPLEADO")
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Empleado idEmpleado;
+
+    public EncabezadoFactura() {
+    }
+
+    public EncabezadoFactura(Integer idEncabezadoFactura) {
+        this.idEncabezadoFactura = idEncabezadoFactura;
+    }
+
+    public Integer getIdEncabezadoFactura() {
+        return idEncabezadoFactura;
+    }
+
+    public void setIdEncabezadoFactura(Integer idEncabezadoFactura) {
+        this.idEncabezadoFactura = idEncabezadoFactura;
+    }
+
+    public Double getValorVenta() {
+        return valorVenta;
+    }
+
+    public void setValorVenta(Double valorVenta) {
+        this.valorVenta = valorVenta;
+    }
+
+    public Double getDescuento() {
+        return descuento;
+    }
+
+    public void setDescuento(Double descuento) {
+        this.descuento = descuento;
+    }
+
+    public Double getSubtotal() {
+        return subtotal;
+    }
+
+    public void setSubtotal(Double subtotal) {
+        this.subtotal = subtotal;
+    }
+
+    public Double getIva() {
+        return iva;
+    }
+
+    public void setIva(Double iva) {
+        this.iva = iva;
+    }
+
+    public Double getTotalVenta() {
+        return totalVenta;
+    }
+
+    public void setTotalVenta(Double totalVenta) {
+        this.totalVenta = totalVenta;
+    }
+
+    @XmlTransient
+    public List<DetalleFactura> getDetalleFacturaList() {
+        return detalleFacturaList;
+    }
+
+    public void setDetalleFacturaList(List<DetalleFactura> detalleFacturaList) {
+        this.detalleFacturaList = detalleFacturaList;
+    }
+
+    public Cliente getIdCliente() {
+        return idCliente;
+    }
+
+    public void setIdCliente(Cliente idCliente) {
+        this.idCliente = idCliente;
+    }
+
+    public Empleado getIdEmpleado() {
+        return idEmpleado;
+    }
+
+    public void setIdEmpleado(Empleado idEmpleado) {
+        this.idEmpleado = idEmpleado;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (idEncabezadoFactura != null ? idEncabezadoFactura.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof EncabezadoFactura)) {
+            return false;
+        }
+        EncabezadoFactura other = (EncabezadoFactura) object;
+        if ((this.idEncabezadoFactura == null && other.idEncabezadoFactura != null) || (this.idEncabezadoFactura != null && !this.idEncabezadoFactura.equals(other.idEncabezadoFactura))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "model.EncabezadoFactura[ idEncabezadoFactura=" + idEncabezadoFactura + " ]";
+    }
+    
+}
