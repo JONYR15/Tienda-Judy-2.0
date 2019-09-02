d3967e3502cb31db3932b5c368b3cc822f0601bf
Bernan Ureña Lopez <bernan-1103@hotmail.com>
2019-09-01 23:25:42 -0600

    Vista de crear facturas

---
+++ TiendaJudy2.0/src/main/java/Utilitarios/AddViewRow.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Utilitarios;
+
+import javax.inject.Named;
+import javax.faces.view.ViewScoped;
+
+/**
+ *
+ * @author Bernan Ureña Lopez
+ */
+@Named(value = "addViewRow")
+@ViewScoped
+public class AddViewRow {
+
+    /**
+     * Creates a new instance of AddViewRow
+     */
+    public AddViewRow() {
+    }
+    
+}
